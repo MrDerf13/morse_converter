405381f01c99dbf6e6052f7edadd90c32b0345e9
MrDerf13 <gia_fred@hotmail.com>
2024-01-03 13:49:57 +1100

    refactored to one function for conversion either way

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # morse_converter
-An html that converts to and from morse code
+-An html that converts to and from morse code
+
+## 02/01/2024 Changes
+- Created all files
+- Basic flex structure created
+- english to morse function created
+
+## ## 03/01/2024 Changes
+- Changed converter to be one funtion based on a language input
+- added language input
--- converter.js
+++ converter.js
@@ -1,12 +1,32 @@
 import { morseCode } from "./morse_dictionary.js";
 
+// <h2 id="morse">English to Morse Converter</h2>
+// <h2 id="english" class="hidden">Morse to English Converter</h2>
+let h1 = document.querySelector("#morse");
+let h2 = document.querySelector("#english");
+h1.classList.toggle("hidden");
+h2.classList.toggle("hidden");
+
 const englishToMorse = (obj) => {
     // grab string from unput value
+    // determine which direction the translation is based on some dropdown
+    let language = document.querySelector(".dropbtn").value;
+
+    if (language == "Morse"){
+        obj = Object.entries(obj).reduce(
+            (acc, [key, value]) => {
+                acc[value] = key;
+                return acc;
+            },
+            {}
+        ); 
+    };
+
     const string = document.querySelector("#string-input").value.toUpperCase();
     let converted = string
-    .split("")
+    .split(language == "Morse" ? " ":"")
     .map((char)=> obj[char])
-    .join(" ")
+    .join(language == "Morse" ? "":" ")
     const output= document.querySelector("#output");
     output.innerHTML = converted;
 }
@@ -14,15 +34,4 @@ const englishToMorse = (obj) => {
 let submitButton = document.querySelector("#string-input");
 submitButton.addEventListener("input", (event)=> {
     englishToMorse(morseCode);
-})
-
-
-const morseToEnglish = (string) => {
-    
-    // grab string from unput value
-    // 
-    // .split("")
-    // .map((char)=>)
-}
-
-// eventlistener "input" for live updates as you type
+});
